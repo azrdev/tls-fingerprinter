26cae24d0858fd02f5c738c25db3ca1f73c7e29b
cmeyer <cmeyer@ab8e1c94-a3be-4edd-a078-c4f91af8f3fd>
2012-05-07 08:55:13 +0000

    git-svn-id: https://hgisvn.nds.ruhr-uni-bochum.de/svn/nds/repos_misc/builder-tasks/SSL@655 ab8e1c94-a3be-4edd-a078-c4f91af8f3fd

--- Stack/src/test/java/de/rub/nds/research/ssl/stack/tests/common/MessageBuilder.java
+++ Stack/src/test/java/de/rub/nds/research/ssl/stack/tests/common/MessageBuilder.java
@@ -1,52 +1,56 @@
 package de.rub.nds.research.ssl.stack.tests.common;
 
-import java.security.InvalidKeyException;
-
 import de.rub.nds.research.ssl.stack.protocols.commons.EConnectionEnd;
 import de.rub.nds.research.ssl.stack.protocols.commons.EProtocolVersion;
 import de.rub.nds.research.ssl.stack.protocols.handshake.ClientHello;
 import de.rub.nds.research.ssl.stack.protocols.handshake.Finished;
-import de.rub.nds.research.ssl.stack.protocols.handshake.datatypes.CipherSuites;
 import de.rub.nds.research.ssl.stack.protocols.handshake.datatypes.MasterSecret;
-import de.rub.nds.research.ssl.stack.protocols.handshake.datatypes.RandomValue;
+import java.security.InvalidKeyException;
 
-/**Builder for SSL handshake messages.
+/**
+ * Builder for SSL handshake messages.
+ *
  * @author Eugen Weiss - eugen.weiss@ruhr-uni-bochum.de
- * @version 0.1
- * Apr 04, 2012
+ * @version 0.1 Apr 04, 2012
  */
 public class MessageBuilder {
 
-	/**Empty public constructor.*/
-	public MessageBuilder() {
-	}
+    /**
+     * Empty public constructor.
+     */
+    public MessageBuilder() {
+    }
 
-	/**Builds a ClientHello message.
-	 * @param protocolVersion SSL/TLS protocol version
-	 * @param random Random value
-	 * @param cipherSuites Cipher suites
-	 * @param compMethod Compression method
-	 * @return ClientHello message
-	 */
-	public final ClientHello createClientHello(byte [] id, 
-			byte [] random, byte [] cipherSuites, byte [] compMethod) {
-		EProtocolVersion protocolVersion = EProtocolVersion.getProtocolVersion(id);
-		ClientHello clientHello = new ClientHello(protocolVersion);
-		clientHello.setRandom(random);
-    	clientHello.setCompressionMethod(compMethod);
-		clientHello.setCipherSuites(cipherSuites);
-		return clientHello;
-	}
-	
-	public final Finished createFinished(final EProtocolVersion protocolVersion, EConnectionEnd endpoint, byte [] handshakeHash, MasterSecret masterSec){
-		Finished finished = new Finished(protocolVersion, endpoint);
-		try {
-			finished.createVerifyData(masterSec, handshakeHash);
-		} catch (InvalidKeyException e) {
-			e.printStackTrace();
-		}
-		finished.encode(true);
-		return finished;
-	}
+    /**
+     * Builds a ClientHello message.
+     *
+     * @param protocolVersion SSL/TLS protocol version
+     * @param random Random value
+     * @param cipherSuites Cipher suites
+     * @param compMethod Compression method
+     * @return ClientHello message
+     */
+    public final ClientHello createClientHello(byte[] id,
+            byte[] random, byte[] cipherSuites, byte[] compMethod) {
+        EProtocolVersion protocolVersion = EProtocolVersion.getProtocolVersion(
+                id);
+        ClientHello clientHello = new ClientHello(protocolVersion);
+        clientHello.setRandom(random);
+        clientHello.setCompressionMethod(compMethod);
+        clientHello.setCipherSuites(cipherSuites);
+        return clientHello;
+    }
 
+    public final Finished createFinished(final EProtocolVersion protocolVersion,
+            EConnectionEnd endpoint, byte[] handshakeHash,
+            MasterSecret masterSec) {
+        Finished finished = new Finished(protocolVersion, endpoint);
+        try {
+            finished.createVerifyData(masterSec, handshakeHash);
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        }
+        finished.encode(true);
+        return finished;
+    }
 }
