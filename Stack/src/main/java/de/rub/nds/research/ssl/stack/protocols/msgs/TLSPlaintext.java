a57d614d4024a315679b53d8cde14125e9252fda
eweiss <eweiss@ab8e1c94-a3be-4edd-a078-c4f91af8f3fd>
2012-05-07 19:22:18 +0000

    Klasse für unverschlüsselte Daten
    git-svn-id: https://hgisvn.nds.ruhr-uni-bochum.de/svn/nds/repos_misc/builder-tasks/SSL@692 ab8e1c94-a3be-4edd-a078-c4f91af8f3fd

---
+++ Stack/src/main/java/de/rub/nds/research/ssl/stack/protocols/msgs/TLSPlaintext.java
@@ -0,0 +1,87 @@
+package de.rub.nds.research.ssl.stack.protocols.msgs;
+
+import de.rub.nds.research.ssl.stack.protocols.ARecordFrame;
+import de.rub.nds.research.ssl.stack.protocols.commons.EContentType;
+import de.rub.nds.research.ssl.stack.protocols.commons.EProtocolVersion;
+
+/**
+ * TLSPlaintext message as defined in Chapter 6.2.1 of RFC 2246
+ * @author Eugen Weiss - eugen.weiss@ruhr-uni-bochum.de
+ * @version 0.1
+ * May 07, 2012
+ */
+public class TLSPlaintext extends ARecordFrame {
+	
+	private byte[] fragment = null;
+	
+	/**
+     * Initializes a plain data record
+     * @param message SSL data record
+     * @param chained Decode single or chained with underlying frames
+     */
+    public TLSPlaintext(final byte[] message, final boolean chained) {
+        // dummy call - decoding will invoke decoders of the parents if desired
+        super();
+        this.decode(message, chained);
+    }
+    
+    /**
+     * Initializes a plain data record
+     * 
+     * @param protocolVersion Protocol version of this message
+     */
+    public TLSPlaintext(final EProtocolVersion protocolVersion) {
+        super(EContentType.APPLICATION, protocolVersion);
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+	public final byte [] encode(final boolean chained) {
+    	byte[] fragment = this.fragment;
+
+    	 super.setPayload(fragment);
+         return chained ? super.encode(true) : fragment;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+	public final void decode(final byte[] message, final boolean chained) {
+    	byte[] tmpBytes;
+        byte[] payloadCopy;
+
+        if (chained) {
+            super.decode(message, true);
+        } else {
+            setPayload(message);
+        }
+
+        // payload already deep copied
+        payloadCopy = getPayload();
+
+        // 1. extract data fragment
+        tmpBytes = new byte[payloadCopy.length];
+        System.arraycopy(payloadCopy, 0, tmpBytes, 0, tmpBytes.length);
+        setFragment(tmpBytes);
+    }
+    
+    /**
+     * Set the data fragment of this record
+     * @param fragment Data fragment
+     */
+    public void setFragment(byte[] fragment) {
+    	this.fragment = fragment;
+    }
+    
+    /**
+     * Get the data fragment of this record
+     * @return Data fragment
+     */
+    public byte[] getFragment() {
+    	return this.fragment;
+    }
+
+}
