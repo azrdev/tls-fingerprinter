c874ee3077eac0c60de5f68732be095d0db99e26
cmeyer <cmeyer@ab8e1c94-a3be-4edd-a078-c4f91af8f3fd>
2012-05-08 07:39:02 +0000

    git-svn-id: https://hgisvn.nds.ruhr-uni-bochum.de/svn/nds/repos_misc/builder-tasks/SSL@694 ab8e1c94-a3be-4edd-a078-c4f91af8f3fd

--- Stack/src/main/java/de/rub/nds/research/ssl/stack/protocols/msgs/TLSPlaintext.java
+++ Stack/src/main/java/de/rub/nds/research/ssl/stack/protocols/msgs/TLSPlaintext.java
@@ -6,16 +6,17 @@ import de.rub.nds.research.ssl.stack.protocols.commons.EProtocolVersion;
 
 /**
  * TLSPlaintext message as defined in Chapter 6.2.1 of RFC 2246
+ *
  * @author Eugen Weiss - eugen.weiss@ruhr-uni-bochum.de
- * @version 0.1
- * May 07, 2012
+ * @version 0.1 May 07, 2012
  */
 public class TLSPlaintext extends ARecordFrame {
-	
-	private byte[] fragment = null;
-	
-	/**
+
+    private byte[] fragment = null;
+
+    /**
      * Initializes a plain data record
+     *
      * @param message SSL data record
      * @param chained Decode single or chained with underlying frames
      */
@@ -24,33 +25,31 @@ public class TLSPlaintext extends ARecordFrame {
         super();
         this.decode(message, chained);
     }
-    
+
     /**
      * Initializes a plain data record
-     * 
+     *
      * @param protocolVersion Protocol version of this message
      */
     public TLSPlaintext(final EProtocolVersion protocolVersion) {
         super(EContentType.APPLICATION, protocolVersion);
     }
-    
+
     /**
      * {@inheritDoc}
      */
     @Override
-	public final byte [] encode(final boolean chained) {
-    	byte[] fragment = this.fragment;
-
-    	 super.setPayload(fragment);
-         return chained ? super.encode(true) : fragment;
+    public final byte[] encode(final boolean chained) {
+        super.setPayload(this.fragment);
+        return chained ? super.encode(true) : fragment;
     }
 
     /**
      * {@inheritDoc}
      */
     @Override
-	public final void decode(final byte[] message, final boolean chained) {
-    	byte[] tmpBytes;
+    public final void decode(final byte[] message, final boolean chained) {
+        byte[] tmpBytes;
         byte[] payloadCopy;
 
         if (chained) {
@@ -67,21 +66,22 @@ public class TLSPlaintext extends ARecordFrame {
         System.arraycopy(payloadCopy, 0, tmpBytes, 0, tmpBytes.length);
         setFragment(tmpBytes);
     }
-    
+
     /**
      * Set the data fragment of this record
+     *
      * @param fragment Data fragment
      */
     public void setFragment(byte[] fragment) {
-    	this.fragment = fragment;
+        this.fragment = fragment;
     }
-    
+
     /**
      * Get the data fragment of this record
+     *
      * @return Data fragment
      */
     public byte[] getFragment() {
-    	return this.fragment;
+        return this.fragment;
     }
-
 }
