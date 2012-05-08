8a5606374160a59c2291f2f62ec4fa58742f7cf8
eweiss <eweiss@ab8e1c94-a3be-4edd-a078-c4f91af8f3fd>
2012-05-08 16:59:32 +0000

    Methoden zum Erstellen eines ClientHello und ClientKeyExchange hinzugefügt
    git-svn-id: https://hgisvn.nds.ruhr-uni-bochum.de/svn/nds/repos_misc/builder-tasks/SSL@701 ab8e1c94-a3be-4edd-a078-c4f91af8f3fd

--- Stack/src/test/java/de/rub/nds/research/ssl/stack/tests/common/MessageBuilder.java
+++ Stack/src/test/java/de/rub/nds/research/ssl/stack/tests/common/MessageBuilder.java
@@ -1,11 +1,23 @@
 package de.rub.nds.research.ssl.stack.tests.common;
 
+import de.rub.nds.research.ssl.stack.protocols.commons.ECipherSuite;
 import de.rub.nds.research.ssl.stack.protocols.commons.EConnectionEnd;
 import de.rub.nds.research.ssl.stack.protocols.commons.EProtocolVersion;
+import de.rub.nds.research.ssl.stack.protocols.commons.KeyExchangeParams;
 import de.rub.nds.research.ssl.stack.protocols.handshake.ClientHello;
+import de.rub.nds.research.ssl.stack.protocols.handshake.ClientKeyExchange;
 import de.rub.nds.research.ssl.stack.protocols.handshake.Finished;
+import de.rub.nds.research.ssl.stack.protocols.handshake.datatypes.CipherSuites;
+import de.rub.nds.research.ssl.stack.protocols.handshake.datatypes.ClientDHPublic;
+import de.rub.nds.research.ssl.stack.protocols.handshake.datatypes.EKeyExchangeAlgorithm;
+import de.rub.nds.research.ssl.stack.protocols.handshake.datatypes.EncryptedPreMasterSecret;
 import de.rub.nds.research.ssl.stack.protocols.handshake.datatypes.MasterSecret;
+import de.rub.nds.research.ssl.stack.protocols.handshake.datatypes.PreMasterSecret;
+
+import java.math.BigInteger;
 import java.security.InvalidKeyException;
+import java.security.PublicKey;
+import java.security.SecureRandom;
 
 /**
  * Builder for SSL handshake messages.
@@ -22,8 +34,7 @@ public class MessageBuilder {
     }
 
     /**
-     * Builds a ClientHello message.
-     *
+     * Builds a ClientHello message on the byte layer
      * @param protocolVersion SSL/TLS protocol version
      * @param random Random value
      * @param cipherSuites Cipher suites
@@ -40,7 +51,90 @@ public class MessageBuilder {
         clientHello.setCipherSuites(cipherSuites);
         return clientHello;
     }
+    
+    /**
+     * Create simple ClientHello containing one cipher suite
+     * @param protocolVersion Protocol version in this message
+     * @return ClientHello message
+     */
+    public final ClientHello createClientHello(final EProtocolVersion protocolVersion) {
+    	//create ClientHello message
+        ClientHello clientHello = new ClientHello(protocolVersion);
 
+        //set the cipher suites
+        CipherSuites suites = new CipherSuites();
+        suites.setSuites(new ECipherSuite[]{
+                    ECipherSuite.TLS_RSA_WITH_AES_128_CBC_SHA});
+        clientHello.setCipherSuites(suites);
+        clientHello.encode(true);
+        return clientHello;
+    }
+    
+    /**
+     * Create simple ClientKeyExchange message depending on the 
+     * key exchange algorithm.
+     * @param protocolVersion
+     * @return
+     */
+    public final ClientKeyExchange createClientKeyExchange(final EProtocolVersion protocolVersion,
+    		final SSLHandshakeWorkflow workflow) {
+    	KeyExchangeParams keyParams = KeyExchangeParams.getInstance();
+    	EKeyExchangeAlgorithm exchangeAlgorithm = keyParams.getKeyExchangeAlgorithm();
+    	PublicKey pk = keyParams.getPublicKey();
+    	ClientKeyExchange cke = new ClientKeyExchange(protocolVersion, exchangeAlgorithm);
+        if (exchangeAlgorithm == EKeyExchangeAlgorithm.RSA) {
+            PreMasterSecret pms = new PreMasterSecret(protocolVersion);
+            //create encoded PMS
+            byte[] encodedPMS = pms.encode(false);
+            //encrypted PreMasterSecret
+            EncryptedPreMasterSecret encPMS = new EncryptedPreMasterSecret(
+                    encodedPMS, pk);
+            workflow.setPreMasterSecret(pms);
+            cke.setExchangeKeys(encPMS);
+        } else {
+            ClientDHPublic clientDHPublic = new ClientDHPublic();
+            byte[] generator = keyParams.getDHGenerator();
+            byte[] primeModulus = keyParams.getDHPrime();
+            byte[] privateValue = new byte[20];
+            byte[] clientPublic = new byte[primeModulus.length];
+            /*
+             * generate a random private value
+             */
+            SecureRandom random = new SecureRandom();
+            random.nextBytes(privateValue);
+
+            BigInteger gen = new BigInteger(1, generator);
+            BigInteger primeMod = new BigInteger(1, primeModulus);
+            BigInteger priv = new BigInteger(1, privateValue);
+
+            /*
+             * compute clients DH public value g^x mod p
+             */
+            clientPublic = gen.modPow(priv, primeMod).toByteArray();
+
+            byte[] tmp = new byte[primeModulus.length];
+
+            if (clientPublic.length > primeModulus.length) {
+                System.arraycopy(clientPublic, 1, tmp, 0, primeModulus.length);
+                clientPublic = tmp;
+            }
+            clientDHPublic.setDhyc(clientPublic);
+            cke.setExchangeKeys(clientDHPublic);
+            PreMasterSecret pms = new PreMasterSecret(privateValue, keyParams.getDhPublic(),
+                    primeModulus);
+            workflow.setPreMasterSecret(pms);
+        }
+    	return cke;
+    }
+
+    /**
+     * Create Finished message
+     * @param protocolVersion Protocol version
+     * @param endpoint Client/Server side
+     * @param handshakeHash Hash of the handshake messages so far
+     * @param masterSec Master secret
+     * @return Finished message
+     */
     public final Finished createFinished(final EProtocolVersion protocolVersion,
             EConnectionEnd endpoint, byte[] handshakeHash,
             MasterSecret masterSec) {
