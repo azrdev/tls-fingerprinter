f74d3b7cde5f14df5ebe599bf1d5ce00ac652246
etews <etews@ab8e1c94-a3be-4edd-a078-c4f91af8f3fd>
2012-05-09 16:56:57 +0000

    Removed useless import.
    git-svn-id: https://hgisvn.nds.ruhr-uni-bochum.de/svn/nds/repos_misc/builder-tasks/SSL@732 ab8e1c94-a3be-4edd-a078-c4f91af8f3fd

--- Stack/src/test/java/de/rub/nds/research/ssl/stack/tests/attacks/BleichenbacherTimingTest.java
+++ Stack/src/test/java/de/rub/nds/research/ssl/stack/tests/attacks/BleichenbacherTimingTest.java
@@ -30,7 +30,6 @@ import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 import javax.crypto.BadPaddingException;
-import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
