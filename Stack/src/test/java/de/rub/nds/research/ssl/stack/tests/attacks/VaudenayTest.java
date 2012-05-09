c5ce3b35d46c0ace10c55106e9a49aa8fc5cf792
etews <etews@ab8e1c94-a3be-4edd-a078-c4f91af8f3fd>
2012-05-09 17:59:45 +0000

    This is expected to work.
    git-svn-id: https://hgisvn.nds.ruhr-uni-bochum.de/svn/nds/repos_misc/builder-tasks/SSL@739 ab8e1c94-a3be-4edd-a078-c4f91af8f3fd

--- Stack/src/test/java/de/rub/nds/research/ssl/stack/tests/attacks/VaudenayTest.java
+++ Stack/src/test/java/de/rub/nds/research/ssl/stack/tests/attacks/VaudenayTest.java
@@ -63,8 +63,8 @@ public class VaudenayTest implements Observer {
     @DataProvider(name = "vaudenay")
     public final Object[][] createData1() {
         return new Object[][]{
-//                    {protocolVersion, false}, //ok case
-                    {protocolVersion, true} //wrong padding
+                    {protocolVersion, false}, //ok case
+//                    {protocolVersion, true} //wrong padding
                 };
     }
     /**
