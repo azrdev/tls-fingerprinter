94dd3303876856d7554a777c5678e6f5b22167c3
cmeyer <cmeyer@ab8e1c94-a3be-4edd-a078-c4f91af8f3fd>
2012-05-14 13:11:19 +0000

    Ausgiebiger MAC Test, dauert sehr lange....
    git-svn-id: https://hgisvn.nds.ruhr-uni-bochum.de/svn/nds/repos_misc/builder-tasks/SSL@784 ab8e1c94-a3be-4edd-a078-c4f91af8f3fd

--- Stack/src/test/java/de/rub/nds/research/ssl/stack/tests/attacks/BleichenbacherTimingTest.java
+++ Stack/src/test/java/de/rub/nds/research/ssl/stack/tests/attacks/BleichenbacherTimingTest.java
@@ -119,7 +119,7 @@ public class BleichenbacherTimingTest implements Observer {
     /**
      *  Number of repetitions.
      */
-    private static final int NUMBER_OF_REPETIIONS = 1000;
+    private static final int NUMBER_OF_REPETIIONS = 100000;
     /**
      *  Detailed Info print out.
      */
