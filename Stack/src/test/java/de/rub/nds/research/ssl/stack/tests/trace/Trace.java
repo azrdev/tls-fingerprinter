d7b631e7f434b44b01afd0dca652099397534e40
cmeyer <cmeyer@ab8e1c94-a3be-4edd-a078-c4f91af8f3fd>
2012-05-14 12:44:28 +0000

    MAC Test
    git-svn-id: https://hgisvn.nds.ruhr-uni-bochum.de/svn/nds/repos_misc/builder-tasks/SSL@782 ab8e1c94-a3be-4edd-a078-c4f91af8f3fd

--- Stack/src/test/java/de/rub/nds/research/ssl/stack/tests/trace/Trace.java
+++ Stack/src/test/java/de/rub/nds/research/ssl/stack/tests/trace/Trace.java
@@ -7,50 +7,50 @@ import de.rub.nds.research.ssl.stack.protocols.ARecordFrame;
 import de.rub.nds.research.ssl.stack.tests.common.SSLHandshakeWorkflow.EStates;
 
 /**
- * Trace information about the SSL handshake processing.
+ *  Trace information about the SSL handshake processing.
  *
- * @author Eugen Weiss - eugen.weiss@ruhr-uni-bochum.de
- * @version 0.1 Apr 10, 2012
+ *  @author Eugen Weiss - eugen.weiss@ruhr-uni-bochum.de
+ *  @version 0.1 Apr 10, 2012
  */
 public final class Trace extends ATrace implements Serializable {
 
     /**
-     * Serial ID.
+     *  Serial ID.
      */
     private static final long serialVersionUID = 1L;
     /**
-     * Newly constructed SSL record.
+     *  Newly constructed SSL record.
      */
     private ARecordFrame currentRecord = null;
     /**
-     * Newly encoded bytes to send *
+     *  Newly encoded bytes to send *
      */
     private byte[] currentRecordBytes = null;
     /**
-     * Original SSL record before manipulation.
+     *  Original SSL record before manipulation.
      */
     private ARecordFrame oldRecord = null;
     /**
-     * Handshake enumeration was used for this record.
+     *  Handshake enumeration was used for this record.
      */
     private boolean isContinued = false;
     /**
-     * Current state in handshake.
+     *  Current state in handshake.
      */
     private EStates state;
 
     /**
-     * Empty constructor.
+     *  Empty constructor.
      */
     public Trace() {
     }
 
     /**
-     * Public constructor of a Trace object.
+     *  Public constructor of a Trace object.
      *
-     * @param currentRecord Newly constructed SSL record
-     * @param oldRecord Original SSL record before manipulation
-     * @param isContinued Handshake enumeration was used for this record
+     *  @param currentRecord Newly constructed SSL record
+     *  @param oldRecord Original SSL record before manipulation
+     *  @param isContinued Handshake enumeration was used for this record
      */
     public Trace(EStates state, final ARecordFrame currentRecord,
             ARecordFrame oldRecord,
@@ -63,13 +63,13 @@ public final class Trace extends ATrace implements Serializable {
     }
 
     /**
-     * Public constructor of a Trace object.
+     *  Public constructor of a Trace object.
      *
-     * @param currentRecord Newly constructed SSL record
-     * @param timestamp Current sending/receiving timestamp of the message
-     * @param oldRecord Original SSL record before manipulation
-     * @param isContinued Handshake enumeration was used for this record
-     * @param nanoTime Current time in nano precision
+     *  @param currentRecord Newly constructed SSL record
+     *  @param timestamp Current sending/receiving timestamp of the message
+     *  @param oldRecord Original SSL record before manipulation
+     *  @param isContinued Handshake enumeration was used for this record
+     *  @param nanoTime Current time in nano precision
      */
     public Trace(EStates state, final ARecordFrame currentRecord,
             final Timestamp timestamp,
@@ -84,96 +84,97 @@ public final class Trace extends ATrace implements Serializable {
     }
 
     /**
-     * Get the current state in handshake.
+     *  Get the current state in handshake.
      *
-     * @return Current state in handshake
+     *  @return Current state in handshake
      */
     public EStates getState() {
         return this.state;
     }
 
     /**
-     * Set the current state.
+     *  Set the current state.
      *
-     * @param state Current state in handshake.
+     *  @param state Current state in handshake.
      */
     public void setState(EStates state) {
         this.state = state;
     }
 
     /**
-     * Get the newly constructed SSL record.
+     *  Get the newly constructed SSL record.
      *
-     * @return Current record
+     *  @return Current record
      */
     public ARecordFrame getCurrentRecord() {
         return currentRecord;
     }
 
     /**
-     * Set the newly constructed SSL record.
+     *  Set the newly constructed SSL record.
      *
-     * @param currentRecord Current record
+     *  @param currentRecord Current record
      */
     public void setCurrentRecord(final ARecordFrame currentRecord) {
         this.currentRecord = currentRecord;
     }
 
     /**
-     * Set current record bytes representation. May differ from currentRecord,
+     *  Set current record bytes representation. May differ from currentRecord,
      * processor decides if currentRecord or currentRecord bytes are processed.
      *
-     * @param currentBytes Current bytes representation.
+     *  @param currentBytes Current bytes representation.
      */
     public void setCurrentRecordBytes(byte[] currentBytes) {
         this.currentRecordBytes = currentBytes.clone();
     }
 
     /**
-     * Get the current record bytes - if manually set. May differ from
+     *  Get the current record bytes - if manually set. May differ from
      * currentRecord, processor decides if currentRecord or currentRecord bytes
      * are processed.
      *
-     * @return Bytes of the current record if set, otherwise null.
+     *  @return Bytes of the current record if set, otherwise null.
      */
     public byte[] getCurrentRecordBytes() {
         byte[] result = null;
-        if(this.currentRecordBytes == null)
+        if (this.currentRecordBytes != null) {
             result = this.currentRecordBytes.clone();
+        }
         return result;
     }
 
     /**
-     * Get original SSL record before manipulation.
+     *  Get original SSL record before manipulation.
      *
-     * @return Old record
+     *  @return Old record
      */
     public ARecordFrame getOldRecord() {
         return oldRecord;
     }
 
     /**
-     * Set original SSL record before manipulation.
+     *  Set original SSL record before manipulation.
      *
-     * @param oldRecord Old record
+     *  @param oldRecord Old record
      */
     public void setOldRecord(final ARecordFrame oldRecord) {
         this.oldRecord = oldRecord;
     }
 
     /**
-     * Shows if handshake enumeration was used for present record.
+     *  Shows if handshake enumeration was used for present record.
      *
-     * @return true if handshake enumeration was used for this record
+     *  @return true if handshake enumeration was used for this record
      */
     public boolean isContinued() {
         return isContinued;
     }
 
     /**
-     * Set present record as handshake enumerated message.
+     *  Set present record as handshake enumerated message.
      *
-     * @param isContinued true if handshake enumeration was used for this record
+     *  @param isContinued true if handshake enumeration was used for this record
      * / false if not
      */
     public void setContinued(final boolean isContinued) {
