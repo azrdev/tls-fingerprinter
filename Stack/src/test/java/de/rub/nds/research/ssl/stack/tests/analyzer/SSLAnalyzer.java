e96b5393b29d667ceec9e3def00f4ccd44add071
eweiss <eweiss@ab8e1c94-a3be-4edd-a078-c4f91af8f3fd>
2012-05-17 16:33:17 +0000

    Java DB einrichten
    git-svn-id: https://hgisvn.nds.ruhr-uni-bochum.de/svn/nds/repos_misc/builder-tasks/SSL@803 ab8e1c94-a3be-4edd-a078-c4f91af8f3fd

--- Stack/src/test/java/de/rub/nds/research/ssl/stack/tests/analyzer/SSLAnalyzer.java
+++ Stack/src/test/java/de/rub/nds/research/ssl/stack/tests/analyzer/SSLAnalyzer.java
@@ -1,52 +1,27 @@
 package de.rub.nds.research.ssl.stack.tests.analyzer;
 
-import java.io.ByteArrayInputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.ObjectInputStream;
 import java.sql.ResultSet;
-import java.util.ArrayList;
 
 import de.rub.nds.research.ssl.stack.Utility;
 import de.rub.nds.research.ssl.stack.tests.analyzer.db.Database;
-import de.rub.nds.research.ssl.stack.tests.trace.Trace;
 
 public class SSLAnalyzer {
 
 	/**
 	 * @param args
 	 */
-	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
-//		InputStream fileInStream = null;
-//		TraceListAnalyzer listAnalyzer = new TraceListAnalyzer();
-//		try {
-//			fileInStream = new FileInputStream("eugenTest.ser");
-//			ObjectInputStream oInput = new ObjectInputStream(fileInStream);
-//			ArrayList<Trace> traceList = (ArrayList<Trace>) oInput.readObject();
-//			listAnalyzer.analyzeList(traceList);
-//			fileInStream.close();
-//		} catch (FileNotFoundException e) {
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		} catch (ClassNotFoundException e) {
-//			e.printStackTrace();
-//		}
 		Database db = new Database();
 		ResultSet result = null;
 		try {
 			result = db.readValue();
 			do {
 				result.next();
-				System.out.println(Utility.byteToHex(result.getBytes("status")));
+				System.out.println(Utility.byteToHex(result.getBytes("value")));
 			}
-			while (!(result.isLast()));
+			while (!(result.next()));
 			
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
