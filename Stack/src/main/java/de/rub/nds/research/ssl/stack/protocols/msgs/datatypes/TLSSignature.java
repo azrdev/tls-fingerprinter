9cff0bc1b7cea5defafb8ff1337183e2038975e5
eweiss <eweiss@ab8e1c94-a3be-4edd-a078-c4f91af8f3fd>
2012-05-17 10:07:47 +0000

    Code aufräumen, FindsBugs, Checkstyle
    git-svn-id: https://hgisvn.nds.ruhr-uni-bochum.de/svn/nds/repos_misc/builder-tasks/SSL@799 ab8e1c94-a3be-4edd-a078-c4f91af8f3fd

--- Stack/src/main/java/de/rub/nds/research/ssl/stack/protocols/msgs/datatypes/TLSSignature.java
+++ Stack/src/main/java/de/rub/nds/research/ssl/stack/protocols/msgs/datatypes/TLSSignature.java
@@ -2,18 +2,8 @@ package de.rub.nds.research.ssl.stack.protocols.msgs.datatypes;
 
 import de.rub.nds.research.ssl.stack.protocols.commons.APubliclySerializable;
 import de.rub.nds.research.ssl.stack.protocols.commons.KeyExchangeParams;
-import de.rub.nds.research.ssl.stack.protocols.commons.SecurityParameters;
 import de.rub.nds.research.ssl.stack.protocols.handshake.
 datatypes.ESignatureAlgorithm;
-
-import java.security.InvalidKeyException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.security.PublicKey;
-import java.security.Signature;
-import java.security.SignatureException;
-import java.security.interfaces.RSAPublicKey;
-import java.util.Arrays;
 
 /**
  * TLS signature as defined in RFC 2246. The signature algorithms DSA and RSA
@@ -37,25 +27,9 @@ public class TLSSignature extends APubliclySerializable {
      */
     private byte[] serverParams;
     /**
-     * Length of a SHA1 hash.
-     */
-    private static final int SHA1_LENGTH = 20;
-
-    /**
-     * Length of a MD5 hash.
-     */
-    private static final int MD5_LENGTH = 16;
-
-    /**
-     * Length of concatenated MD5 and SHA1 hash.
-     */
-    private static final int CONCAT_HASH_LENGTH = SHA1_LENGTH + MD5_LENGTH;
-
-    /**
      * Max. count of parameters in ServerKeyExchange message.
      */
     private static final int MAX_COUNT_PARAM = 4;
-
     /**
      * Signature value.
      */
@@ -92,135 +66,15 @@ public class TLSSignature extends APubliclySerializable {
     public final boolean checkSignature(final byte[] signature) {
         KeyExchangeParams keyParams = KeyExchangeParams.getInstance();
         boolean valid = false;
+        ISignature sign;
         if (this.sigAlgorithm == ESignatureAlgorithm.RSA) {
-            valid = checkRSASignature(signature, keyParams.getPublicKey());
+            sign = new RSASignature(getServerParams());
+            valid = sign.checkSignature(signature, keyParams.getPublicKey());
         } else {
-            valid = checkDSSSignature(signature, keyParams.getPublicKey());
+            sign = new DSASignature(getServerParams());
+            valid = sign.checkSignature(signature, keyParams.getPublicKey());
         }
         return valid;
-    }
-
-    /**
-     * Check a RSA signed message. If RSA was used to sign a message, the
-     * message is first hashed with MD5 and SHA1. Afterwards the signature is
-     * applied
-     *
-     * @param signature Signature bytes
-     * @param pk Public key
-     * @return True if signature verification was successful
-     */
-    public final boolean checkRSASignature(final byte[] signature,
-            final PublicKey pk) {
-        SecurityParameters params = SecurityParameters.getInstance();
-        byte[] clientRandom = params.getClientRandom();
-        byte[] serverRandom = params.getServerRandom();
-        byte[] parameters = getServerParams();
-        byte[] md5Hash;
-        byte[] sha1Hash;
-        byte[] concat = new byte[CONCAT_HASH_LENGTH];
-        md5Hash = md5Hash(clientRandom, serverRandom,
-                parameters);
-        sha1Hash = sha1Hash(clientRandom, serverRandom,
-                parameters);
-        //concatenate the two hashes
-        int pointer = 0;
-        System.arraycopy(md5Hash, 0, concat, pointer, md5Hash.length);
-        pointer += md5Hash.length;
-        System.arraycopy(sha1Hash, 0, concat, pointer, sha1Hash.length);
-        //compute signature
-        byte[] msg = null;
-        if (pk != null && pk instanceof RSAPublicKey) {
-            RSAPublicKey rsaPK = (RSAPublicKey) pk;
-            msg = RsaUtil.pubOp(signature, rsaPK);
-        }
-        byte[] recHash = new byte[CONCAT_HASH_LENGTH];
-        if (msg != null) {
-            System.arraycopy(msg, msg.length - recHash.length, recHash, 0,
-                    recHash.length);
-        }
-        return Arrays.equals(recHash, concat);
-    }
-
-    /**
-     * Check a DSS signed message. If RSA was used to sign a message, the
-     * message is first hashed with SHA1. Afterwards the signature is applied.
-     *
-     * @param signature Signature bytes
-     * @param pk Public key
-     * @return True if signature verification was successful
-     */
-    public final boolean checkDSSSignature(final byte[] signature,
-            final PublicKey pk) {
-        boolean valid = false;
-        SecurityParameters params = SecurityParameters.getInstance();
-        byte[] clientRandom = params.getClientRandom();
-        byte[] serverRandom = params.getServerRandom();
-        byte[] parameters = getServerParams();
-        Signature sig;
-        try {
-            sig = Signature.getInstance("SHA1withDSA");
-            sig.initVerify(pk);
-            sig.update(clientRandom);
-            sig.update(serverRandom);
-            sig.update(parameters);
-            valid = sig.verify(signature);
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (InvalidKeyException e) {
-            e.printStackTrace();
-        } catch (SignatureException e) {
-            e.printStackTrace();
-        }
-        return valid;
-    }
-
-    /**
-     * Generate a MD5 Hash for the signature.
-     *
-     * @param clientRandom Client random parameter
-     * @param serverRandom Server random parameter
-     * @param parameters Server parameters
-     * @return MD5 hash
-     */
-    public final byte[] md5Hash(final byte[] clientRandom,
-            final byte[] serverRandom, final byte[] parameters) {
-        byte[] result = null;
-        MessageDigest md5 = null;
-        try {
-            md5 = MessageDigest.getInstance("MD5");
-            md5.update(clientRandom);
-            md5.update(serverRandom);
-            md5.update(parameters);
-            result = md5.digest();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        return result;
-    }
-
-    /**
-     * Generate a SHA1 Hash for the signature.
-     *
-     * @param clientRandom Client random parameter
-     * @param serverRandom Server random parameter
-     * @param parameters Server parameters
-     * @return SHA1 hash
-     */
-    public final byte[] sha1Hash(final byte[] clientRandom,
-            final byte[] serverRandom, final byte[] parameters) {
-        byte[] result = null;
-        MessageDigest sha1 = null;
-        try {
-            sha1 = MessageDigest.getInstance("SHA");
-            sha1.update(clientRandom);
-            sha1.update(serverRandom);
-            sha1.update(parameters);
-            result = sha1.digest();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-
-        return result;
     }
 
     /**
@@ -231,7 +85,8 @@ public class TLSSignature extends APubliclySerializable {
         /*
          * To be implemented.
          */
-        return null;
+        byte [] tmp = new byte[0];
+        return tmp;
     }
 
     /**
