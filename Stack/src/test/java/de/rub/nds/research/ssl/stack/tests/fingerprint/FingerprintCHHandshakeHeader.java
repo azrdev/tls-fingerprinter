cad52ad6f8532d7e7ae736013afe46400b32eb34
eweiss <eweiss@ab8e1c94-a3be-4edd-a078-c4f91af8f3fd>
2012-06-05 19:09:56 +0000

    Fingerprint Tests
    git-svn-id: https://hgisvn.nds.ruhr-uni-bochum.de/svn/nds/repos_misc/builder-tasks/SSL@883 ab8e1c94-a3be-4edd-a078-c4f91af8f3fd

---
+++ Stack/src/test/java/de/rub/nds/research/ssl/stack/tests/fingerprint/FingerprintCHHandshakeHeader.java
@@ -0,0 +1,180 @@
+package de.rub.nds.research.ssl.stack.tests.fingerprint;
+
+import java.io.IOException;
+import java.util.Observable;
+import java.util.Observer;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import de.rub.nds.research.ssl.stack.protocols.commons.ECipherSuite;
+import de.rub.nds.research.ssl.stack.protocols.commons.EProtocolVersion;
+import de.rub.nds.research.ssl.stack.protocols.handshake.ClientHello;
+import de.rub.nds.research.ssl.stack.protocols.handshake.datatypes.CipherSuites;
+import de.rub.nds.research.ssl.stack.protocols.handshake.datatypes.RandomValue;
+import de.rub.nds.research.ssl.stack.tests.analyzer.RecordHeaderParameters;
+import de.rub.nds.research.ssl.stack.tests.analyzer.TestHashAnalyzer;
+import de.rub.nds.research.ssl.stack.tests.analyzer.common.AFingerprintAnalyzer;
+import de.rub.nds.research.ssl.stack.tests.common.MessageBuilder;
+import de.rub.nds.research.ssl.stack.tests.common.SSLHandshakeWorkflow;
+import de.rub.nds.research.ssl.stack.tests.common.TestConfiguration;
+import de.rub.nds.research.ssl.stack.tests.common.SSLHandshakeWorkflow.EStates;
+import de.rub.nds.research.ssl.stack.tests.trace.Trace;
+import de.rub.nds.research.ssl.stack.tests.workflows.ObservableBridge;
+
+/**
+ * Fingerprint the Client Hello handshake header. Perform Tests by
+ * manipulating the message type, protocol version and length
+ * bytes in the header.
+ * @author Eugen Weiss - eugen.weiss@ruhr-uni-bochum.de
+ * @version 0.1
+ * May 31, 2012
+ */
+public class FingerprintCHHandshakeHeader implements Observer {
+	
+	 /**
+    * Handshake workflow to observe.
+    */
+   private SSLHandshakeWorkflow workflow;
+   /**
+    * Test host.
+    */
+   private static final String HOST = "localhost";
+   /**
+    * Test port.
+    */
+   private static final int PORT = 9443;
+   /**
+    * Test counter.
+    */
+   private int counter = 1;
+   /**
+    * Defualt protocol version.
+    */
+   private EProtocolVersion protocolVersion = EProtocolVersion.TLS_1_0;
+   
+   /**
+    * Test parameters
+    */
+   private RecordHeaderParameters parameters = new RecordHeaderParameters();
+   
+   static Logger logger = Logger.getRootLogger();
+   
+   @BeforeClass
+   public void setUp() {
+   	PropertyConfigurator.configure("logging.properties");
+   }
+   
+   @DataProvider(name = "chHandHeader")
+   public Object[][] createData1() {
+	   return new Object[][]{
+			   {"Wrong message type", new byte[]{(byte)0xff},
+				   null, null},
+			   {"Invalid protocol version 0xff,0xff",
+				   null, new byte[]{(byte)0xff,(byte)0xff},null},
+			   {"Invalid protocol version 0x00,0x00",
+				   null, new byte[]{(byte)0x00,(byte)0x00},null},
+			   {"Invalid protocol version SSLv3",
+				   null, new byte[]{(byte)0x03,(byte)0x00},null},
+			   {"Invalid protocol version TLSv1.2",
+				   null, new byte[]{(byte)0x03,(byte)0x03},null},
+			   {"Invalid length 0x00,0x00,0x00",
+				   null, null, new byte[]{(byte)0x00,(byte)0x00,(byte)0x00}},
+			   {"Invalid length 0xff,0xff,0xff",
+				   null, null, new byte[]{(byte)0xff,(byte)0xff,(byte)0xff}},
+	   };
+   }
+   
+   @Test(enabled = true, dataProvider = "chHandHeader", invocationCount = 1)
+   public void manipulateCHHandshakeHeader(String desc, byte [] msgType,
+		   byte [] protocolVersion, byte [] recordLength) {
+	   logger.info("++++Start Test No." + counter + "(" + desc +")++++");
+	   logger.info("Following test parameters are used:");
+       workflow = new SSLHandshakeWorkflow();
+       if (TestConfiguration.HOST.isEmpty() || TestConfiguration.PORT == 0) {
+       	workflow.connectToTestServer(HOST, PORT);
+       	logger.info("Test Server: " + HOST +":" +PORT);
+       }
+       else {
+       	workflow.connectToTestServer(TestConfiguration.HOST,
+       			TestConfiguration.PORT);
+       	logger.info("Test Server: " + TestConfiguration.HOST +":" + TestConfiguration.PORT);
+       }
+       workflow.addObserver(this, EStates.CLIENT_HELLO);
+       logger.info(EStates.CLIENT_HELLO.name() + " state is observed");
+       
+       parameters.setMsgType(msgType);
+       parameters.setProtocolVersion(protocolVersion);
+       parameters.setRecordLength(recordLength);
+       parameters.setTestClassName(this.getClass().getName());
+       
+       workflow.start();
+       
+       AFingerprintAnalyzer analyzer = new TestHashAnalyzer(parameters);
+       analyzer.analyze(workflow.getTraceList());
+       
+       this.counter++;
+       logger.info("++++Test finished.++++");
+   }
+   
+   /**
+    * Update observed object.
+    *
+    * @param o Observed object
+    * @param arg Arguments
+    */
+   @Override
+   public void update(Observable o, Object arg) {
+       MessageBuilder msgBuilder = new MessageBuilder();
+       Trace trace = null;
+       EStates states = null;
+       ObservableBridge obs;
+       if (o instanceof ObservableBridge) {
+           obs = (ObservableBridge) o;
+           states = (EStates) obs.getState();
+           trace = (Trace) arg;
+       }
+       if (states == EStates.CLIENT_HELLO) {
+    	   ECipherSuite[] suites = new ECipherSuite[]{
+    			   ECipherSuite.TLS_RSA_WITH_AES_128_CBC_SHA};
+    	   CipherSuites cipherSuites = new CipherSuites();
+    	   cipherSuites.setSuites(suites);
+    	   RandomValue random = new RandomValue();
+    	   byte [] compMethod = new byte[]{0x00};
+    	   ClientHello clientHello = msgBuilder.createClientHello(this.protocolVersion.getId(),
+    			   random.encode(false), cipherSuites.encode(false), compMethod);
+    	   byte [] payload = clientHello.encode(true);
+    	   if (parameters.getMsgType() != null) {
+    		   byte [] msgType = parameters.getMsgType();
+    		   System.arraycopy(msgType, 0, payload, 5, msgType.length);
+    	   }
+    	   if (parameters.getRecordLength() != null) {
+    		   byte [] recordLength = parameters.getRecordLength();
+    		   System.arraycopy(recordLength, 0, payload, 6, recordLength.length);
+    	   }
+    	   if (parameters.getProtocolVersion() != null) {
+    		   byte [] protVersion = parameters.getProtocolVersion();
+    		   System.arraycopy(protVersion, 0, payload, 9, protVersion.length);
+    	   }
+    	   trace.setCurrentRecordBytes(payload);
+    	   trace.setCurrentRecord(clientHello);
+       }
+   }
+   
+   /**
+    * Close the Socket after the test run.
+    */
+   @AfterMethod
+   public void tearDown() {
+       try {
+           workflow.closeSocket();
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
+   }
+
+}
--- Stack/src/test/java/de/rub/nds/research/ssl/stack/tests/fingerprint/FingerprintClientHello.java
+++ Stack/src/test/java/de/rub/nds/research/ssl/stack/tests/fingerprint/FingerprintClientHello.java
@@ -4,20 +4,28 @@ import java.io.IOException;
 import java.util.Observable;
 import java.util.Observer;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import de.rub.nds.research.ssl.stack.Utility;
 import de.rub.nds.research.ssl.stack.protocols.commons.ECipherSuite;
 import de.rub.nds.research.ssl.stack.protocols.commons.EProtocolVersion;
+import de.rub.nds.research.ssl.stack.protocols.commons.StackProperties;
 import de.rub.nds.research.ssl.stack.protocols.handshake.ClientHello;
 import de.rub.nds.research.ssl.stack.protocols.handshake.datatypes.CipherSuites;
 import de.rub.nds.research.ssl.stack.protocols.handshake.datatypes.RandomValue;
-import de.rub.nds.research.ssl.stack.tests.analyzer.common.TraceListAnalyzer;
+import de.rub.nds.research.ssl.stack.tests.analyzer.ClientHelloParameters;
+import de.rub.nds.research.ssl.stack.tests.analyzer.RecordHeaderParameters;
+import de.rub.nds.research.ssl.stack.tests.analyzer.TestHashAnalyzer;
+import de.rub.nds.research.ssl.stack.tests.analyzer.common.AFingerprintAnalyzer;
 import de.rub.nds.research.ssl.stack.tests.common.MessageBuilder;
 import de.rub.nds.research.ssl.stack.tests.common.SSLHandshakeWorkflow;
+import de.rub.nds.research.ssl.stack.tests.common.TestConfiguration;
 import de.rub.nds.research.ssl.stack.tests.common.SSLHandshakeWorkflow.EStates;
-import de.rub.nds.research.ssl.stack.tests.common.SSLTestUtils;
 import de.rub.nds.research.ssl.stack.tests.trace.Trace;
 import de.rub.nds.research.ssl.stack.tests.workflows.ObservableBridge;
 
@@ -33,16 +41,7 @@ public class FingerprintClientHello implements Observer {
      * Handshake workflow to observe.
      */
     private SSLHandshakeWorkflow workflow;
-    /**
-     * Help utilities for testing.
-     */
-    private SSLTestUtils utils = new SSLTestUtils();
-    /**
-     * Patterns to create suite batch.
-     */
-    private String[] patterns1 = {"TLS_DHE_DSS", "TLS_DH_DSS"};
-    private String[] patterns2 = {"TLS_DH_anon"};
-    private String[] patterns3 = {"TLS"};
+
     /**
      * Test host.
      */
@@ -56,21 +55,60 @@ public class FingerprintClientHello implements Observer {
      */
     private int counter = 1;
     /**
+     * Defualt protocol version.
+     */
+    private EProtocolVersion protocolVersion = EProtocolVersion.TLS_1_0;
+    
+    /**
      * Test parameters.
      */
-    private byte[] protVersion;
-    private byte[] random;
-    private byte[] cipherSuites;
-    private byte[] compMethod;
-    /**
-     * Test constants
-     */
-    private final ECipherSuite[] suites1 = new ECipherSuite[]{
-        ECipherSuite.TLS_RSA_WITH_AES_128_CBC_SHA};
-    private final ECipherSuite[] suites2 = utils.constructSuiteBatch(patterns1);
-    private final ECipherSuite[] suites3 = utils.constructSuiteBatch(patterns2);
-    private final ECipherSuite[] suites4 = new ECipherSuite[]{};
-    private final ECipherSuite[] suites5 = utils.constructSuiteBatch(patterns3);
+    private ClientHelloParameters parameters = new ClientHelloParameters();
+    
+    static Logger logger = Logger.getRootLogger();
+    
+    byte [] sessionID = new byte []{(byte)0xff,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,
+    		(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,
+    		(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,
+    		(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,
+    		(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,
+    		(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,
+    		(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,
+    		(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,
+    		(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,
+    		(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,
+    		(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,
+    		(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,
+    		(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,
+    		(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,
+    		(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,
+    		(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,
+    		(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,
+    		(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,
+    		(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,
+    		(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,
+    		(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,
+    		(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,
+    		(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,
+    		(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,
+    		(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,
+    		(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,
+    		(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,
+    		(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,
+    		(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,
+    		(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,
+    		(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,
+    		(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,
+    		(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,
+    		(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,
+    		(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,
+    		(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,
+    		(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f,(byte)0x0f
+    };
+    
+    @BeforeClass
+    public void setUp() {
+    	PropertyConfigurator.configure("logging.properties");
+    }
 
     /**
      * Test parameters for ClientHello fingerprinting.
@@ -79,55 +117,63 @@ public class FingerprintClientHello implements Observer {
      */
     @DataProvider(name = "clientHello")
     public Object[][] createData1() {
-        //byte [] random1 = new byte[]{0x00,0x00,0x00,0x00,0x00};
-        RandomValue random2 = new RandomValue();
         return new Object[][]{
-                    {"OK case", EProtocolVersion.TLS_1_0.getId(),
-                        random2.encode(false), suites1, new byte[]{(byte) 0x00}},
-                    {"Protocol version SSL 3.0",
-                        EProtocolVersion.SSL_3_0.getId(),
-                        random2.encode(false), suites1, new byte[]{(byte) 0x00}},
-                    {"Protocol version TLS 1.2",
-                        EProtocolVersion.TLS_1_2.getId(),
-                        random2.encode(false), suites1, new byte[]{(byte) 0x00}},
-                    {"TLS_DHE_DSS and TLS_DH_DSS Cipher suites",
-                        EProtocolVersion.TLS_1_0.getId(),
-                        random2.encode(false), suites2, new byte[]{(byte) 0x00}},
-                    {"Compression method 0x01", EProtocolVersion.TLS_1_0.getId(),
-                        random2.encode(false), suites1, new byte[]{(byte) 0x01}},
-                    {"No cipher suites defined",
-                        EProtocolVersion.TLS_1_0.getId(),
-                        random2.encode(false), suites4, new byte[]{(byte) 0x00}},
-                    {"No cipher but comp-method 0x01", EProtocolVersion.TLS_1_0.
-                        getId(),
-                        random2.encode(false), suites4, new byte[]{(byte) 0x01}},};
+        			{"No session ID defined but value is set to 0xff", new byte[]{(byte)0xff},
+        				null, null, null, null},
+        			{"256 Byte sessionID", null,
+        				sessionID, null, null, null},
+            		{"256 Byte sessionID and sessionID length 0x00", null,
+            			sessionID, new byte[]{(byte)0x00}, null, null},
+                    {"Compression method 0xa1", null , null, null,
+                        null, new byte[]{(byte) 0xa1}},
+                    {"Wrong value for cipher suite length 0x01", null , null, null,
+                        new byte[]{(byte) 0x01}, null},
+                    {"Wrong value for cipher suite length 0x00", null , null, null,
+                        new byte[]{(byte) 0x00}, null},
+                    };
     }
 
     /**
-     * Start SSL handshake.
-     *
-     * @throws InterruptedException
-     * @throws IOException
+     * Manipulate Client Hello message to perform fingerprinting tests
+     * @param desc Test description
+     * @param protocolVersion TLS protocol version
+     * @param random Random value
+     * @param suites Cipher suites
+     * @param compMethod Compression method
      */
     @Test(enabled = true, dataProvider = "clientHello", invocationCount = 1)
-    public void fingerprintClientHello(String desc, byte[] protocolVersion,
-            byte[] random, ECipherSuite[] suites, byte[] compMethod) {
+    public void fingerprintClientHello(String desc,
+            byte[] noSessionValue, byte [] session, byte[] sessionIdLength,
+            byte [] cipherLength, byte[] compMethod) {
+    	StackProperties.getInstance();
+    	logger.info("++++Start Test No." + counter + "(" + desc +")++++");
         workflow = new SSLHandshakeWorkflow();
-        workflow.connectToTestServer(HOST, PORT);
+        if (TestConfiguration.HOST.isEmpty() || TestConfiguration.PORT == 0) {
+        	workflow.connectToTestServer(HOST, PORT);
+        	logger.info("Test Server: " + HOST +":" +PORT);
+        }
+        else {
+        	workflow.connectToTestServer(TestConfiguration.HOST,
+        			TestConfiguration.PORT);
+        	logger.info("Test Server: " + TestConfiguration.HOST +":" + TestConfiguration.PORT);
+        }
         workflow.addObserver(this, EStates.CLIENT_HELLO);
-        CipherSuites cipherSuites = new CipherSuites();
-        cipherSuites.setSuites(suites);
-        this.protVersion = protocolVersion;
-        this.cipherSuites = cipherSuites.encode(false);
-        this.random = random;
-        this.compMethod = compMethod;
+        logger.info(EStates.CLIENT_HELLO.name() + " state is observed");
+        parameters.setNoSessionIdValue(noSessionValue);
+        parameters.setSessionId(session);
+        parameters.setSessionIdLen(sessionIdLength);
+        parameters.setCipherLen(cipherLength);
+        parameters.setCompMethod(compMethod);
+        parameters.setTestClassName(this.getClass().getName());
         workflow.start();
-        System.out.println("Test No." + this.counter + " : " + desc);
-        TraceListAnalyzer analyze = new TraceListAnalyzer();
-        analyze.logOutput(workflow.getTraceList());
-        System.out.println("------------------------------");
+        
+        AFingerprintAnalyzer analyzer = new TestHashAnalyzer(parameters);
+        analyzer.analyze(workflow.getTraceList());
+        
         this.counter++;
+        logger.info("++++Test finished.++++");
     }
+    
 
     /**
      * Update observed object.
@@ -137,20 +183,48 @@ public class FingerprintClientHello implements Observer {
      */
     @Override
     public void update(Observable o, Object arg) {
-        MessageBuilder msgBuilder = new MessageBuilder();
-        Trace trace = null;
-        EStates states = null;
-        ObservableBridge obs;
-        if (o instanceof ObservableBridge) {
-            obs = (ObservableBridge) o;
-            states = (EStates) obs.getState();
-            trace = (Trace) arg;
-        }
-        if (states == EStates.CLIENT_HELLO) {
-            ClientHello clientHello = msgBuilder.createClientHello(protVersion,
-                    random, cipherSuites, compMethod);
-            trace.setCurrentRecord(clientHello);
-        }
+    	MessageBuilder msgBuilder = new MessageBuilder();
+    	Trace trace = null;
+    	EStates states = null;
+    	ObservableBridge obs;
+    	if (o instanceof ObservableBridge) {
+    		obs = (ObservableBridge) o;
+    		states = (EStates) obs.getState();
+    		trace = (Trace) arg;
+    	}
+    	if (states == EStates.CLIENT_HELLO) {
+    		ECipherSuite[] suites = new ECipherSuite[]{
+    				ECipherSuite.TLS_RSA_WITH_AES_128_CBC_SHA};
+    		CipherSuites cipherSuites = new CipherSuites();
+    		cipherSuites.setSuites(suites);
+    		RandomValue random = new RandomValue();
+    		byte [] compMethod = new byte[]{0x00};
+    		ClientHello clientHello = msgBuilder.createClientHello(this.protocolVersion.getId(),
+    				random.encode(false), cipherSuites.encode(false), compMethod);
+    		byte [] payload;
+    		if (parameters.getSessionId() != null) {
+    			byte [] session = parameters.getSessionId();
+    			clientHello.setSessionID(session);
+    		}
+    		if (parameters.getCompMethod() != null) {
+    			clientHello.setCompressionMethod(parameters.getCompMethod());
+    		}
+    		payload = clientHello.encode(true);
+    		if (parameters.getNoSessionIdValue() != null) {
+    			byte [] value = parameters.getNoSessionIdValue();
+    			System.arraycopy(value, 0, payload, payload.length - 7, value.length);
+    		}
+    		if (parameters.getSessionIdLen() != null) {
+    			byte [] sLen = parameters.getSessionIdLen();
+    			System.arraycopy(sLen, 0, payload, 43, sLen.length);
+    		}
+    		if (parameters.getCipherLen() != null) {
+    			byte [] cLen = parameters.getCipherLen();
+    			System.arraycopy(cLen, 0, payload, payload.length - 5, cLen.length);
+    		}
+    		trace.setCurrentRecordBytes(payload);
+    		trace.setCurrentRecord(clientHello);
+    	}
     }
 
     /**
