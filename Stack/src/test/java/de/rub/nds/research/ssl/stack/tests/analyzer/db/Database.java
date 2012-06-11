f0323d03ab8e61b9dcdddbb78bd8e1cf3e59fbe9
eweiss <eweiss@ab8e1c94-a3be-4edd-a078-c4f91af8f3fd>
2012-06-11 12:56:06 +0000

    git-svn-id: https://hgisvn.nds.ruhr-uni-bochum.de/svn/nds/repos_misc/builder-tasks/SSL@893 ab8e1c94-a3be-4edd-a078-c4f91af8f3fd

--- Stack/src/test/java/de/rub/nds/research/ssl/stack/tests/analyzer/db/Database.java
+++ Stack/src/test/java/de/rub/nds/research/ssl/stack/tests/analyzer/db/Database.java
@@ -34,10 +34,10 @@ public class Database {
     }
 	
 	public void connectDB() throws Exception {
-//		Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
-//		conn = DriverManager.getConnection("jdbc:derby:Fingerprint;create=false;user=tester;password=ssltest");
-		Class.forName("org.apache.derby.jdbc.ClientDriver");
-		conn = DriverManager.getConnection("jdbc:derby://localhost:1527//home/regit/svn/SSL/Stack/Fingerprint;create=false;user=tester;password=ssltest");
+		Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+		conn = DriverManager.getConnection("jdbc:derby:Fingerprint;create=false;user=tester;password=ssltest");
+//		Class.forName("org.apache.derby.jdbc.ClientDriver");
+//		conn = DriverManager.getConnection("jdbc:derby://localhost:1527//home/regit/svn/SSL/Stack/Fingerprint;create=false;user=tester;password=ssltest");
 	}
 	
 	public ResultSet findHashInDB(String hash) {
