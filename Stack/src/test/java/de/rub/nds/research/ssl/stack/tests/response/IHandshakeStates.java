1e0b1677dd8f616a0a46f84a696b28394f374c22
cmeyer <cmeyer@ab8e1c94-a3be-4edd-a078-c4f91af8f3fd>
2012-07-25 08:50:53 +0000

    git-svn-id: https://hgisvn.nds.ruhr-uni-bochum.de/svn/nds/repos_misc/builder-tasks/SSL@1036 ab8e1c94-a3be-4edd-a078-c4f91af8f3fd

--- Stack/src/test/java/de/rub/nds/research/ssl/stack/tests/response/IHandshakeStates.java
+++
@@ -1,9 +0,0 @@
-package de.rub.nds.research.ssl.stack.tests.response;
-
-import de.rub.nds.research.ssl.stack.protocols.handshake.AHandshakeRecord;
-
-public interface IHandshakeStates {
-	
-	public void handleResponse(AHandshakeRecord handRecord);
-
-}
