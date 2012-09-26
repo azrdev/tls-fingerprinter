f64cd770867c4889c80facb62f606934749db47c
etews <etews@ab8e1c94-a3be-4edd-a078-c4f91af8f3fd>
2012-09-26 11:46:42 +0000

    Added a warning, assignment has no effect.
    git-svn-id: https://hgisvn.nds.ruhr-uni-bochum.de/svn/nds/repos_misc/builder-tasks/SSL@1171 ab8e1c94-a3be-4edd-a078-c4f91af8f3fd

--- VirtualNetworkLayer/src/main/java/de/rub/nds/virtualnetworklayer/socket/VNLSocketImpl.java
+++ VirtualNetworkLayer/src/main/java/de/rub/nds/virtualnetworklayer/socket/VNLSocketImpl.java
@@ -77,6 +77,7 @@ class VNLSocketImpl extends SocketImpl {
     @Override
     protected void bind(final InetAddress host, final int port) throws
             IOException {
+    	// TODO: this has no effect!
         this.address = address;
         this.port = port;
     }
