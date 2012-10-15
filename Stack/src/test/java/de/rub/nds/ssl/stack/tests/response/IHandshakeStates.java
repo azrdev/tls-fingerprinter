18b1348a393b759ace6aabe5686cfed48b3f617e
cmeyer <cmeyer@ab8e1c94-a3be-4edd-a078-c4f91af8f3fd>
2012-10-15 10:36:56 +0000

    * Code Umstrukturierung - Workflows sind jetzt Teil des Stacks, nicht der Tests
    git-svn-id: https://hgisvn.nds.ruhr-uni-bochum.de/svn/nds/repos_misc/builder-tasks/SSL@1219 ab8e1c94-a3be-4edd-a078-c4f91af8f3fd

--- Stack/src/test/java/de/rub/nds/ssl/stack/tests/response/IHandshakeStates.java
+++
@@ -1,8 +0,0 @@
-package de.rub.nds.ssl.stack.tests.response;
-
-import de.rub.nds.ssl.stack.protocols.handshake.AHandshakeRecord;
-
-public interface IHandshakeStates {
-
-    public void handleResponse(AHandshakeRecord handRecord);
-}
