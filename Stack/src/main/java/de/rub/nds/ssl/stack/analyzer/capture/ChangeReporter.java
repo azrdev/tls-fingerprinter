98f7298fbcdb76dfa4df7cbd6a5d229bc22de74b
cmeyer <cmeyer@ab8e1c94-a3be-4edd-a078-c4f91af8f3fd>
2012-12-14 15:07:45 +0000

    * Analyzer Projekt hinzugefuegt
    git-svn-id: https://hgisvn.nds.ruhr-uni-bochum.de/svn/nds/repos_misc/builder-tasks/SSL@1443 ab8e1c94-a3be-4edd-a078-c4f91af8f3fd

--- Stack/src/main/java/de/rub/nds/ssl/stack/analyzer/capture/ChangeReporter.java
+++
@@ -1,8 +0,0 @@
-package de.rub.nds.ssl.stack.analyzer.capture;
-
-import java.util.List;
-
-public interface ChangeReporter {
-	public void reportChange(ClientHelloFingerprint chf, ServerFingerprint sf, List<ServerFingerprint> previousResponses);
-
-}
--- Stack/src/main/java/de/rub/nds/ssl/stack/analyzer/capture/ServerResponseFingerprint.java
+++
@@ -1,5 +0,0 @@
-package de.rub.nds.ssl.stack.analyzer.capture;
-
-public class ServerResponseFingerprint {
-
-}
