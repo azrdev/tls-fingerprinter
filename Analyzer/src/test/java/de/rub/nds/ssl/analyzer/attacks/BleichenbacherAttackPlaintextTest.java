55c92c683582b5344fe327ff1e372153208fb83a
cmeyer <cmeyer@ab8e1c94-a3be-4edd-a078-c4f91af8f3fd>
2013-01-15 14:00:28 +0000

    * Analyzer Umstrukturierung
    git-svn-id: https://hgisvn.nds.ruhr-uni-bochum.de/svn/nds/repos_misc/builder-tasks/SSL@1475 ab8e1c94-a3be-4edd-a078-c4f91af8f3fd

--- Analyzer/src/test/java/de/rub/nds/ssl/analyzer/attacks/BleichenbacherAttackPlaintextTest.java
+++ Analyzer/src/test/java/de/rub/nds/ssl/analyzer/attacks/BleichenbacherAttackPlaintextTest.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package de.rub.nds.ssl.analyzer.attacks;
 
 import de.rub.nds.ssl.analyzer.attacks.bleichenbacher.oracles.AOracle;
