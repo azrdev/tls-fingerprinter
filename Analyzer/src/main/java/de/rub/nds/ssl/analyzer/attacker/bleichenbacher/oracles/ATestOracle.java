c30af2cb27f656d6c77c3852240e63ca1e1dd8d8
jsomorovsky <jsomorovsky@ab8e1c94-a3be-4edd-a078-c4f91af8f3fd>
2013-02-25 18:48:54 +0000

    some forgotten commits - TestOracle angepasst (JSSE oracle war nicht korrekt implementiert)
    git-svn-id: https://hgisvn.nds.ruhr-uni-bochum.de/svn/nds/repos_misc/builder-tasks/SSL@1696 ab8e1c94-a3be-4edd-a078-c4f91af8f3fd

--- Analyzer/src/main/java/de/rub/nds/ssl/analyzer/attacker/bleichenbacher/oracles/ATestOracle.java
+++ Analyzer/src/main/java/de/rub/nds/ssl/analyzer/attacker/bleichenbacher/oracles/ATestOracle.java
@@ -114,41 +114,63 @@ public abstract class ATestOracle extends AOracle {
             return false;
         }
     }
-    
+
     /**
      * JSSE bug Plaintext oracle, for testing purposes:
-     * 
-     * Example for 256/512 byte long RSA key:
-     * The oracle returns true if:
-     * <ul>
-     *   <li>first two bytes are equal to 0x00 0x02</li>
-     *   <li>the following 8 bytes do not contain 0x00</li>
-     *   <li>the following (l-48-80) bytes contain at least one 0x00 byte,
-     *   where l is the message/key length</li>
-     * </ul>
-     * 
+     *
+     * Example for 256/512 byte long RSA key: The oracle returns true if: <ul>
+     * <li>first two bytes are equal to 0x00 0x02</li> <li>the following 8 bytes
+     * do not contain 0x00</li> <li>the following (l-48-80) bytes contain at
+     * least one 0x00 byte, where l is the message/key length</li> </ul>
+     *
      * @param msg
-     * @return 
+     * @return
      */
     private boolean checkJSSE(byte[] msg) {
-        if(msg.length <= 128) {
-            throw new UnsupportedOperationException("The oracle not supported for"
-                    + "keys < 1024 bits.");
-        }
+        
         // check first 8 bytes
-        if(! checkSecond(msg)) {            
+        if (!checkSecond(msg)) {
             return false;
         }
-        // check the following bytes (excluding the last PMS and 80 padding bytes)
-        int last = msg.length - 1 - 48 - 80;
-        for (int i=9; i< last; i++) {
-            if (msg[i] == 0x00) {
-                // if message contains 0x00 in one of the following bytes, our
-                // oracle returns an Internal error
+        // check if the second last byte is equal to 0x00
+        if(msg[msg.length-2] == 0x00) {
+            if(!containsByte((byte)0x00, msg, 10, msg.length-2)) {
                 return true;
+            }
+        }
+        
+        if (msg.length > 128) {
+            // check the following bytes (excluding the last PMS and 80 padding bytes)
+            int last = msg.length - 1 - 48 - 80;
+            for (int i = 9; i < last; i++) {
+                if (msg[i] == 0x00) {
+                    // if message contains 0x00 in one of the following bytes, our
+                    // oracle returns an Internal error
+                    return true;
+                }
             }
         }
         // otherwise, no Internal error is returned
         return false;
     }
+    
+    /**
+     * checks if the message contains byte b in the area between 
+     * <from,to>
+     * 
+     * @param b
+     * @param msg
+     * @param from
+     * @param to
+     * @return 
+     */
+    private boolean containsByte(byte b, byte[] msg, int from, int to) {
+        for (int i = from; i < to; i++) {
+            if (msg[i] == b) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
---
+++ Analyzer/src/test/java/de/rub/nds/ssl/analyzer/attacks/BleichenbacherOracleTest.java
@@ -0,0 +1,72 @@
+package de.rub.nds.ssl.analyzer.attacks;
+
+import de.rub.nds.ssl.analyzer.attacker.Bleichenbacher;
+import de.rub.nds.ssl.analyzer.attacker.BleichenbacherCrypto12;
+import de.rub.nds.ssl.analyzer.attacker.bleichenbacher.oracles.AOracle;
+import de.rub.nds.ssl.analyzer.attacker.bleichenbacher.oracles.ATestOracle;
+import de.rub.nds.ssl.analyzer.attacker.bleichenbacher.oracles.StdPlainOracle;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.SecureRandom;
+import java.security.Security;
+import java.util.Collections;
+import java.util.LinkedList;
+import javax.crypto.Cipher;
+import junit.framework.Assert;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author Juraj Somorovsky - juraj.somorovsky@rub.de
+ * @version 0.1
+ */
+public class BleichenbacherOracleTest {
+
+    /**
+     * Initialize the log4j logger.
+     */
+    static Logger logger = Logger.getRootLogger();
+
+    @Test(enabled = true)
+    public final void testBleichenbacherAttackPerformanceTrimmers()
+            throws Exception {
+
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(1024);
+        KeyPair keyPair = keyPairGenerator.genKeyPair();
+        AOracle oracle = new StdPlainOracle(keyPair.getPublic(),
+                        ATestOracle.OracleType.JSSE, 128);
+        
+        byte[] msg = new byte[127];
+        for(int i=0; i<msg.length; i++) {
+            msg[i] = 0x01;
+        }
+        // start with 0x02, no 0x00 byte given
+        msg[0] = 0x02;
+        
+        Assert.assertFalse(oracle.checkPKCSConformity(msg));
+        
+        // set the second last byte to 0x00
+        msg[msg.length-2] = 0x00;        
+        Assert.assertTrue(oracle.checkPKCSConformity(msg));
+        
+        // insert an extra 0x00 byte in the middle
+        msg[20] = 0x00;
+        Assert.assertFalse(oracle.checkPKCSConformity(msg));
+    }
+
+    /**
+     * Initialize logging properties
+     */
+    @BeforeClass
+    public void setUpClass() {
+        PropertyConfigurator.configure("logging.properties");
+        logger.info("##################################");
+        logger.info(this.getClass().getSimpleName());
+        logger.info("##################################");
+    }
+}
