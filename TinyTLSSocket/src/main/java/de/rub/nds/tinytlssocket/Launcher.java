3f45c2ab82dd04028a16bb370f995b91964657a0
cmeyer <cmeyer@ab8e1c94-a3be-4edd-a078-c4f91af8f3fd>
2013-03-25 14:16:01 +0000

    + Tool zur Berechnung zyklischer Gruppen
    git-svn-id: https://hgisvn.nds.ruhr-uni-bochum.de/svn/nds/repos_misc/builder-tasks/SSL@1743 ab8e1c94-a3be-4edd-a078-c4f91af8f3fd

---
+++ TinyTLSSocket/src/main/java/de/rub/nds/tinytlssocket/Launcher.java
@@ -0,0 +1,39 @@
+package de.rub.nds.tinytlssocket;
+
+/**
+ * Server launcher.
+ *
+ * @author Christopher Meyer - christopher.meyer@rub.de
+ * @version 0.1
+ *
+ * Mar 25, 2013
+ */
+public class Launcher {
+
+    /**
+     * TinyTLSServer launcher
+     *
+     * @param args key store path, password, protocol version, port
+     */
+    public static void main(String[] args) throws Exception {
+        final TLSServer serverThread;
+        if (args.length != 4) {
+            System.out.println("Invalid number of arguments!\n"
+                    + "Usage: java -jar TinyTLSServer.jar "
+                    + " Key store path, Password, Protocol version, Port");
+        } else {
+            int port = Integer.parseInt(args[3]);
+            serverThread = new TLSServer(args[0], args[1], args[2], port);
+            serverThread.start();
+
+            Runtime.getRuntime().addShutdownHook(new Thread() {
+                @Override
+                public void run() {
+                    if (serverThread != null) {
+                        serverThread.shutdown();
+                    }
+                }
+            });
+        }
+    }
+}
