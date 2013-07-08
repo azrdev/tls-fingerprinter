501615372e5b9f691f0a0874f6dc78e1e3a4575e
jsomorovsky <jsomorovsky@ab8e1c94-a3be-4edd-a078-c4f91af8f3fd>
2013-07-08 13:20:19 +0000

    GroupManager anpassungen reingenommen
    git-svn-id: https://hgisvn.nds.ruhr-uni-bochum.de/svn/nds/repos_misc/builder-tasks/SSL@1988 ab8e1c94-a3be-4edd-a078-c4f91af8f3fd

--- BleichenbacherTests/src/test/java/de/rub/nds/ssl/analyzer/attacks/BleichenbacherAttackPlaintextTest.java
+++ BleichenbacherTests/src/test/java/de/rub/nds/ssl/analyzer/attacks/BleichenbacherAttackPlaintextTest.java
@@ -42,6 +42,41 @@ public class BleichenbacherAttackPlaintextTest {
      */
     static Logger logger = Logger.getRootLogger();
     /**
+     * Plain PKCS 1024 bit
+     */
+    private static final byte[] plainPKCS1024 = new byte[]{
+        (byte) 0x00, (byte) 0x02,
+        (byte) 0x01, (byte) 0x01, (byte) 0xc0, (byte) 0xff, (byte) 0xee,
+        (byte) 0xba, (byte) 0xbe, (byte) 0xc0, (byte) 0xff, (byte) 0xee,
+        (byte) 0xba, (byte) 0xbe, (byte) 0xc0, (byte) 0xff, (byte) 0xee,
+        (byte) 0xba, (byte) 0xbe, (byte) 0xc0, (byte) 0xff, (byte) 0xee,
+        (byte) 0xba, (byte) 0xbe, (byte) 0xc0, (byte) 0xff, (byte) 0xee,
+        (byte) 0xba, (byte) 0xbe, (byte) 0xc0, (byte) 0xff, (byte) 0xee,
+        (byte) 0xba, (byte) 0xbe, (byte) 0xc0, (byte) 0xff, (byte) 0xee,
+        (byte) 0xba, (byte) 0xbe, (byte) 0xc0, (byte) 0xff, (byte) 0xee,
+        (byte) 0xba, (byte) 0xbe, (byte) 0xc0, (byte) 0xff, (byte) 0xee,
+        (byte) 0xba, (byte) 0xbe, (byte) 0xc0, (byte) 0xff, (byte) 0xee,
+        (byte) 0xba, (byte) 0xbe, (byte) 0xc0, (byte) 0xff, (byte) 0xee,
+        (byte) 0xba, (byte) 0xbe, (byte) 0xc0, (byte) 0xff, (byte) 0xee,
+        (byte) 0xba, (byte) 0xbe, (byte) 0xc0, (byte) 0xff, (byte) 0xee,
+        (byte) 0xba, (byte) 0xbe, (byte) 0xc0, (byte) 0xff, (byte) 0xee,
+        (byte) 0xba, (byte) 0xbe, (byte) 0xc0, (byte) 0xff, (byte) 0xee,
+        (byte) 0xba, (byte) 0xbe,
+        (byte) 0x00,
+        (byte) 0x03, (byte) 0x08, // TLS 1.2 = 03 03
+        (byte) 0x01, (byte) 0x02, (byte) 0x03, (byte) 0x04, (byte) 0x05,
+        (byte) 0x06, (byte) 0x07, (byte) 0x08, (byte) 0x09, (byte) 0x0a,
+        (byte) 0x0b, (byte) 0x0c, (byte) 0x0d, (byte) 0x0e, (byte) 0x0f,
+        (byte) 0x10, (byte) 0x11, (byte) 0x12, (byte) 0x13, (byte) 0x14,
+        (byte) 0x15, (byte) 0x16, (byte) 0x17, (byte) 0x18, (byte) 0x19,
+        (byte) 0x1a, (byte) 0x1b, (byte) 0x1c, (byte) 0x1d, (byte) 0x1e,
+        (byte) 0x1f, (byte) 0x20, (byte) 0x21, (byte) 0x22, (byte) 0x23,
+        (byte) 0x24, (byte) 0x25, (byte) 0x26, (byte) 0x27, (byte) 0x28,
+        (byte) 0x29, (byte) 0x2a, (byte) 0x2b, (byte) 0x2c, (byte) 0x2d,
+        (byte) 0x2e
+    };
+    
+    /**
      * Plain PKCS message
      */
     private static final byte[] plainPKCS = new byte[]{
@@ -314,19 +349,19 @@ public class BleichenbacherAttackPlaintextTest {
         logger.warn("Max:       " + queriesBardou.get(iterations - 1));
     }
 
-    @Test(enabled = false)
+    @Test(enabled = true)
     public final void testBleichenbacherAttackStaticKey()
             throws Exception {
         Security.addProvider(new BouncyCastleProvider());
-        KeyStore ks = loadKeyStore(new FileInputStream("2048.jks"),
+        KeyStore ks = loadKeyStore(new FileInputStream("1024.jks"),
                 "password");
 
-        byte[] plainBytes = plainPKCS;
+        byte[] plainBytes = plainPKCS1024;
 
-        AOracle oracle = new StdPlainOracle(ks.getCertificate("2048_rsa").
-                getPublicKey(), ATestOracle.OracleType.TTT, 256);
+        AOracle oracle = new StdPlainOracle(ks.getCertificate("1024_rsa").
+                getPublicKey(), ATestOracle.OracleType.TTT, 128);
 
-        Bleichenbacher attacker = new Bleichenbacher(plainBytes,
+        BleichenbacherCrypto12 attacker = new BleichenbacherCrypto12(plainBytes,
                 oracle, true);
         attacker.attack();
     }
