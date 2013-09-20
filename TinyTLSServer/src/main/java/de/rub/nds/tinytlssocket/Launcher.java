ff7ecb9dd2491210022558f31bc5500af2289479
cmeyer <cmeyer@ab8e1c94-a3be-4edd-a078-c4f91af8f3fd>
2013-09-20 11:53:13 +0000

    git-svn-id: https://hgisvn.nds.ruhr-uni-bochum.de/svn/nds/repos_misc/builder-tasks/SSL@2225 ab8e1c94-a3be-4edd-a078-c4f91af8f3fd

--- TinyTLSServer/src/main/java/de/rub/nds/tinytlssocket/Launcher.java
+++ TinyTLSServer/src/main/java/de/rub/nds/tinytlssocket/Launcher.java
@@ -1,7 +1,9 @@
 package de.rub.nds.tinytlssocket;
 
 import java.security.Provider;
+import java.security.Provider.Service;
 import java.security.Security;
+import sun.security.ec.SunEC;
 
 /**
  * Server launcher.
@@ -20,9 +22,10 @@ public class Launcher {
      */
     public static void main(String[] args) throws Exception {
         final TLSServer serverThread;
-//        Security.addProvider(new SunEC());
-        for(Provider provider : Security.getProviders()) {
-            System.out.println(provider);
+        Provider sunEC = new SunEC();
+        Security.addProvider(sunEC);
+        for(Service service : sunEC.getServices()) {
+            System.out.println(service.getAlgorithm());
         }
 //args = new String[]{"/mnt/dataEnc/repos/repos_misc/cmeyer/code/FixedDHCertificates/FixedECDH.jks", "ecdh", "TLS", "51707", "true"};        
         if (args.length != 5) {
